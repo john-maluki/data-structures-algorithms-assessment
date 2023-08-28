d6e498089b21a5c37d4596f36c3c614f20bb6945
john <johnmaluki58@gmail.com>
2023-08-28 12:00:10 +0300

    Complete question 1

--- questions/question_1.py
+++ questions/question_1.py
@@ -15,3 +15,25 @@
     print(is_balanced(expression1))  # Output: True
     print(is_balanced(expression2))  # Output: False
 """
+from collections import deque
+
+def is_balanced(expression):
+    stack = deque()
+    opening_brackets = "([{"
+    closing_brackets = ")]}"
+    bracket_pairs = {")": "(", "}": "{", "]": "["}
+
+    for char in expression:
+        if char in opening_brackets:
+            stack.append(char)
+        elif char in closing_brackets:
+            if len(stack) == 0:
+                return False
+            if stack[-1] == bracket_pairs[char]:
+                stack.pop()
+     
+    return len(stack) == 0
+
+expression1 = "({)"
+
+print(is_balanced(expression1))
